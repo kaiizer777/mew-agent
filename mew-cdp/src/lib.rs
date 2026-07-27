a32682819e5756701f8a63b8628d2914d50abddf
sufiyan733 <kanekigaming786@gmail.com>
2026-07-28 03:27:22 +0530

    Initial scaffold for mew-agent workspace

---
+++ mew-cdp/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn init() {
+    // mew-cdp stub
+}
---
+++ mew-cli/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    mew_cdp::init();
+    mew_perception::init();
+    mew_agent::init();
+    println!("mew-cli initialized");
+}
